ab3577165d8c37069995b55a740a24d5eb9f749e
KomaR1 <sypachov@gmail.com>
2020-05-04 14:36:51 +0300

    Fixed errors

--- youtube/models.py
+++ youtube/models.py
@@ -10,6 +10,9 @@ class CustomUser(AbstractUser):
 class Genre(models.Model):
     genre = models.CharField(max_length=20, help_text='Enter a video genre', unique=True)
 
+    def __str__(self):
+        return self.genre
+
     class Meta:
         verbose_name = 'genre'
         verbose_name_plural = 'genres'
